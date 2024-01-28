f9fa91ea1e22ef285156a6ca33584197a553b780
Clemente Sepulveda <clemente.seplveda@uc.cl>
2024-01-28 15:41:37 -0300

    feature/daily price

---
+++ update_daily_price.py
@@ -0,0 +1,19 @@
+import os 
+import pytz
+import datetime
+import pandas as pd
+
+timezone = pytz.timezone('Chile/Continental')
+now = datetime.datetime.now(tz = timezone)
+current_date = now.strftime("%d_%m_%y")
+
+# print(now)
+df = pd.DataFrame()
+for dir in os.listdir('data'):
+    if current_date != dir.split(' ')[0]:
+        continue 
+    
+    df = pd.concat([df, pd.read_csv(f'data/{dir}')])
+
+data = df.groupby('name').agg(pd.Series.mode)[['price', 'discount']]
+data.to_csv(f'daily/{current_date}.csv')
